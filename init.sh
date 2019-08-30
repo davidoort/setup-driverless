9baf33623a1c5e3722c3705351ba3bea80db6992
David Oort Alonso <davidoort@hotmail.com>
2019-08-30 17:47:03 +0200

    executable version up

--- init.sh
+++ init.sh
@@ -12,22 +12,6 @@ echo "Installing Python3-PIP" $'\n'
 sudo apt-get install python3-pip
 
 
-## Install ROS Melodic - http://wiki.ros.org/melodic/Installation/Ubuntu
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-
-sudo apt install ros-melodic-desktop-full
-
-sudo rosdep init
-rosdep update
-
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
-
 ## Git
 
 function blank_line {
@@ -61,7 +45,34 @@ blank_line
 
 
 
+## Install ROS Melodic - http://wiki.ros.org/melodic/Installation/Ubuntu
+# First add the ROS repository by going to "Software and Updates" in Applications, making sure that the four checkboxes in Ubuntu Software are ticked and that Canonical Partners is enabled in Other Software
+
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install ros-melodic-desktop-full
+
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+#sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get install python-rosinstall -y
+
+
 ### Optional things. 
+
+
+## Gazebo
+
+# You will need to add the osrfoundation repo to your Linux package system in order to get the new packages of Gazebo.
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+#sudo apt-get update
+
 
 
 ## Vim
@@ -78,5 +89,4 @@ blank_line
 
 
 ### Other personal preferences such as Atom, Chrome, etc.
-
 
