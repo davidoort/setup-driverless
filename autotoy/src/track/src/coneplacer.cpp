435b99c5734c8194898b0a459519b5fdfb494f5d
Benjamin Keltjens <B.D.Keltjens@student.tudelft.nl>
2019-09-13 00:55:37 +0200

    untested cone node, correct implementation of .msg types

--- autotoy/src/track/src/coneplacer.cpp
+++ autotoy/src/track/src/coneplacer.cpp
@@ -1,84 +1,87 @@
 #include "ros/ros.h"
+#include "track/ConePlacer.h"
+#include "track/Track.h" //includes both the lines and cones object
+// #include "track/Cones.h"
+// #include "track/Line.h"
 #include <math.h>
 #include <iostream>
-#include <vector>
-#include <tuple>
 
-
-std::tuple<std::vector<std::vector<float>>,std::vector<std::vector<float>>>  conePlacer(const std::vector<std::vector<float>>  &centreline, const float conedistance) //centreline is a 2D std::vector of points (x,y)
-//returns 2 std::vector objects in the same shape for leftcones and right cones
+struct ConesInternal
 {
-  const int length =  centreline.size(); //find length of centreline array
-  std::vector<std::vector<float>> leftcones(length, std::vector<float>(2,0)), rightcones(length, std::vector<float>(2,0)); //create return arrays each an array of two points
+  track::Cones leftcones;
+  track::Cones rightcones;
+}
+
+void  conePlacer(const track::Line &centreline, const float conedistance, ConesInternal &conesobj) //centreline is line class from generator, conedistance describes cones displacement from centre
+// conesobj is reference to temporary holder for cone arrays || cones are changed by reference so no return values
+{
+  const int length =  centreline.points.size(); //find length of centreline array
   
   //init values for temporary calculations in for loop
   std::vector<float> direction(2); //initalise direction vector for each point on the centre line
   float directionmag; //magnitude of direcvector
   std::vector<float> leftconedirection(2);
 
+  track::Point* leftpoint; // to store temporary point for left cones
+  track::Point* rightpoint; // to store temporary point for right cones
+
   for(int i = 0; i < length; i++)
   {
     // check if at beginning, middle, or end of the array
     if(i == 0) //first point
     {
-      direction[0] = centreline[1][0] - centreline[0][0]; //change in x
-      direction[1] = centreline[1][1] - centreline[0][1]; //change in y
+      direction[0] = centreline.points[1].x - centreline.points[0].x //change in x
+      direction[1] = centreline.points[1].y - centreline.points[0].y; //change in y
     }
 
     else if(i == length-1) //final point
     {
-      direction[0] = centreline[length][0] - centreline[length-1][0]; //change in x
-      direction[1] = centreline[length][1] - centreline[length-1][1]; //change in y
+      direction[0] = centreline.points[length].x - centreline.points[length-1].x; //change in x
+      direction[1] = centreline.points[length].y - centreline.points[length-1].y; //change in y
     }
 
     else //middle point
     {
-      direction[0] = centreline[i+1][0] - centreline[i-1][0]; //change in x
-      direction[1] = centreline[i+1][1] - centreline[i-1][1]; //change in y
+      direction[0] = centreline.points[i+1].x - centreline.points[i-1].x; //change in x
+      direction[1] = centreline.points[i+1].y- centreline.points[i-1].y; //change in y
     }
 
   directionmag = sqrt(direction[0]*direction[0] + direction[1]*direction[1]); //find magnitude of direction vector
   leftconedirection = {-direction[1]/directionmag, direction[0]/directionmag}; //rotate direction vector 90 degrees counter clockwise and convert to unit vector
   
-  leftcones[i] = {centreline[i][0] + leftconedirection[0]*conedistance, centreline[i][1] + leftconedirection[1]*conedistance}; //find displacement from centreline point for left cone
-  rightcones[i] = {centreline[i][0] - leftconedirection[0]*conedistance, centreline[i][1] - leftconedirection[1]*conedistance}; //find displacement from centreline point for the right cone
+  leftpoint->x = (float)(centreline.points[i].x + leftconedirection[0]*conedistance); // push x,y points of left cone to temporary point object holder
+  leftpoint->y = (float)(centreline.points[i].y + leftconedirection[1]*conedistance);
 
-  return {leftcones, rightcones};    
+  rightpoint->x = (float)(centreline.points[i].x - leftconedirection[0]*conedistance); // push x,y points of right cone to temporary point object holder
+  rightpoint->y = (float)(centreline.points[i].x - leftconedirection[1]*conedistance);
+
+  conesobj.leftcones.cones.push_back(*leftpoint); //append leftpoint to cones obj
+  conesobj.rightcones.cones.push_back(*rightpoint); //append rightpoint to cones obj
+
+  std::cout << "Cones Calculated" << std::endl;
   }
 }
 
-struct Cones
+bool server(track::ConePlacer::Request  &req, track::ConePlacer::Response &res)
 {
-  std::vector<std::vector<float>> leftcones;// = new std::vector<std::vector<float>>;
-  std::vector<std::vector<float>> rightcones;// = new std::vector<std::vector<float>>;
-};
+  ConesInternal* conesptr = new ConesInternal; // add ConesInternal to the heap (where can I delete it?)
+  conePlacer(req.centreline, 1.5, *conesptr); // fill Cones objects in conesptr object
+  res.leftcones = (*conesptr).leftcones; // fill response with left cones
+  res.rightcones = (*conesptr).rightcones; // fille response with rightcones
+  ROS_INFO("Generated cone placements in response to ConePlacer-request");
+  return true;
+}
+        
 
 
-
-int main()//int argc, char **argv)
+int main(int argc, char **argv)
 {
-  // ros::init(argc, argv, "coneplacer");
-  // ros::NodeHandle n;
-  // ros::spin();
+  ros::init(argc, argv, "coneplacer"); //initialise node
+  ros::NodeHandle n; //node variable
 
-  //example line
-  std::vector<std::vector<float>> line(10, std::vector<float>(2,0));
-
-  float holderx = 0;
-  float holdery = 0;
-  for(int i = 0; i < line.size(); i++)
-  {
-    line[i][0] = holderx;
-    line[i][1] = holdery;
-    holderx += 2;
-    holdery += 1;
-  }
-
-
-  //Cones* result = new Cones;
-  Cones result = conePlacer(line, 0.2);
-  //std::cout << (*leftcones)[0][1] << std::endl;
-  delete result;
+  ros::ServiceServer service = n.advertiseService("/track/coneplacer", server); // create server for coneplacer
+  ROS_INFO("Ready to generate cones.");
+  ros::spin();
 
   return 0;
 }
