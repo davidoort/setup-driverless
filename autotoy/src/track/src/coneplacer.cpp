5ebb9f4e804d197e738c9253babce56d561039de
Benjamin Keltjens <B.D.Keltjens@student.tudelft.nl>
2019-09-13 13:21:35 +0200

    server with /simulation/main.cpp , generator.cpp, coneplacer.cpp working

--- autotoy/src/track/src/coneplacer.cpp
+++ autotoy/src/track/src/coneplacer.cpp
@@ -10,27 +10,34 @@ struct ConesInternal
 {
   track::Cones leftcones;
   track::Cones rightcones;
-}
+};
+
+struct PointInternal
+{
+  float x;
+  float y;
+};
 
 void  conePlacer(const track::Line &centreline, const float conedistance, ConesInternal &conesobj) //centreline is line class from generator, conedistance describes cones displacement from centre
 // conesobj is reference to temporary holder for cone arrays || cones are changed by reference so no return values
 {
+  std::cout << "conePlacer started" << std::endl;
   const int length =  centreline.points.size(); //find length of centreline array
   
   //init values for temporary calculations in for loop
-  std::vector<float> direction(2); //initalise direction vector for each point on the centre line
+  std::vector<float> direction = {(float)0,(float)0}; //initalise direction vector for each point on the centre line
   float directionmag; //magnitude of direcvector
-  std::vector<float> leftconedirection(2);
+  std::vector<float> leftconedirection = {(float)0,(float)0};
 
-  track::Point* leftpoint; // to store temporary point for left cones
-  track::Point* rightpoint; // to store temporary point for right cones
+  track::Point* leftpoint = new track::Point; // to store temporary point for left cones
+  track::Point* rightpoint = new track::Point; // to store temporary point for right cones
 
-  for(int i = 0; i < length; i++)
+  for(int i = 1; i < length; i++)
   {
     // check if at beginning, middle, or end of the array
     if(i == 0) //first point
     {
-      direction[0] = centreline.points[1].x - centreline.points[0].x //change in x
+      direction[0] = centreline.points[1].x - centreline.points[0].x; //change in x
       direction[1] = centreline.points[1].y - centreline.points[0].y; //change in y
     }
 
@@ -46,26 +53,32 @@ void  conePlacer(const track::Line &centreline, const float conedistance, ConesI
       direction[1] = centreline.points[i+1].y- centreline.points[i-1].y; //change in y
     }
 
-  directionmag = sqrt(direction[0]*direction[0] + direction[1]*direction[1]); //find magnitude of direction vector
-  leftconedirection = {-direction[1]/directionmag, direction[0]/directionmag}; //rotate direction vector 90 degrees counter clockwise and convert to unit vector
-  
-  leftpoint->x = (float)(centreline.points[i].x + leftconedirection[0]*conedistance); // push x,y points of left cone to temporary point object holder
-  leftpoint->y = (float)(centreline.points[i].y + leftconedirection[1]*conedistance);
+    directionmag = (float)(sqrt(direction[0]*direction[0] + direction[1]*direction[1])); //find magnitude of direction vector
+    leftconedirection[0] = -(direction[1])/directionmag;
+    leftconedirection[1] = (direction[0])/directionmag;
+    // leftconedirection = {-direction[1]/directionmag, direction[0]/directionmag}; //rotate direction vector 90 degrees counter clockwise and convert to unit vector
+    
+    leftpoint->x = (float)(centreline.points[i].x + leftconedirection[0]*conedistance); // push x,y points of left cone to temporary point object holder
+    leftpoint->y = (float)(centreline.points[i].y + leftconedirection[1]*conedistance);
 
-  rightpoint->x = (float)(centreline.points[i].x - leftconedirection[0]*conedistance); // push x,y points of right cone to temporary point object holder
-  rightpoint->y = (float)(centreline.points[i].x - leftconedirection[1]*conedistance);
+    rightpoint->x = (float)(centreline.points[i].x - leftconedirection[0]*conedistance); // push x,y points of right cone to temporary point object holder
+    rightpoint->y = (float)(centreline.points[i].x - leftconedirection[1]*conedistance);
+    
 
-  conesobj.leftcones.cones.push_back(*leftpoint); //append leftpoint to cones obj
-  conesobj.rightcones.cones.push_back(*rightpoint); //append rightpoint to cones obj
+    conesobj.leftcones.cones.push_back(*leftpoint); //append leftpoint to cones obj
+    conesobj.rightcones.cones.push_back(*rightpoint); //append rightpoint to cones obj
 
-  std::cout << "Cones Calculated" << std::endl;
+    
   }
+
+  delete leftpoint, rightpoint;
+  std::cout << "Cones Calculated" << std::endl;
 }
 
 bool server(track::ConePlacer::Request  &req, track::ConePlacer::Response &res)
 {
   ConesInternal* conesptr = new ConesInternal; // add ConesInternal to the heap (where can I delete it?)
-  conePlacer(req.centreline, 1.5, *conesptr); // fill Cones objects in conesptr object
+  conePlacer(req.centreline, (float)1.5, *conesptr); // fill Cones objects in conesptr object
   res.leftcones = (*conesptr).leftcones; // fill response with left cones
   res.rightcones = (*conesptr).rightcones; // fille response with rightcones
   ROS_INFO("Generated cone placements in response to ConePlacer-request");
--- autotoy/src/track/src/generator.cpp
+++ autotoy/src/track/src/generator.cpp
@@ -1,29 +1,68 @@
 #include "ros/ros.h"
 #include "track/Generator.h"
+#include "track/ConePlacer.h"
 #include "track/Track.h"
+// to use a client node within a server node go to https://answers.ros.org/question/197868/can-a-node-be-a-subscriber-and-client-at-the-same-time/
 
-bool add(track::Generator::Request  &req,
-         track::Generator::Response &res)
-{
-  std::vector<track::Point> centrelinepoints = {*(new track::Point()), *(new track::Point())};
-  track::Line* centreline = new track::Line();
-  centreline->points = centrelinepoints;
 
-  track::Track* output = new track::Track();
-  output->centreline = *(centreline);
-  res.track = *output;
-  ROS_INFO("Generated new track in response to generator-request");
-  return true;
-}
+//declare some variables that need to be defined before generateTrack is defined
+bool generateTrack(track::Generator::Request  &req, track::Generator::Response &res);
+ros::ServiceClient *clientPtr; //pointer for a client
+
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "generator");
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("/track/generate", add);
+  ros::ServiceClient client = n.serviceClient<track::ConePlacer>("/track/coneplacer"); //create client needed to request from cones
+  clientPtr = &client; //give the address of the client to the clientPtr
+
+  ros::ServiceServer service = n.advertiseService("/track/generate", generateTrack);
+
   ROS_INFO("Ready to generate tracks.");
   ros::spin();
 
   return 0;
+}
+
+bool generateTrack(track::Generator::Request  &req,
+         track::Generator::Response &res)
+{
+  // create centreline
+  int numberofpoints = 5;
+  std::vector<track::Point> my_array(numberofpoints);
+  for(int i = 0; i < numberofpoints; i++)
+  {
+    my_array[i].x = (float)(i*1+3);
+    my_array[i].y = (float)(i*2+4);
+  }
+
+  std::vector<track::Point> centrelinepoints = my_array;
+  track::Line* centreline = new track::Line();
+  centreline->points = centrelinepoints;
+
+  //set up client made in main and request data from ConePlacer
+  ros::ServiceClient client = (ros::ServiceClient)*clientPtr; //dereference the clientPtr
+  track::ConePlacer srv;
+  srv.request.centreline = *centreline;
+  if (client.call(srv))
+  {
+    ROS_INFO("requested cones from centreline generator");
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service from coneplacer");
+  }
+
+
+  //create and fill node response object (track)
+  track::Track* output = new track::Track();
+  output->centreline = *(centreline);
+  output->leftcones = srv.response.leftcones;
+  output->rightcones = srv.response.rightcones;
+  res.track = *output;
+  ROS_INFO("Generated new track in response to generator-request");
+  return true;
 }
