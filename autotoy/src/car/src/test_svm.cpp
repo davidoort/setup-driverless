8323f163c336d389a26741d6a88840b85ff56b6a
Alejandro Barberia <alejandrobarberia@gmail.com>
2019-09-14 20:53:23 +0200

    update SVM, testing function works, real function needs to be tested

---
+++ autotoy/src/car/src/test_svm.cpp
@@ -0,0 +1,54 @@
+#include <opencv2/core.hpp>
+#include <opencv2/ml.hpp>
+#include "ros/ros.h"
+#include "track/Cone.h"
+#include "track/Cones.h"
+#include "track/Line.h"
+
+
+int main(int argc, char **argv) {
+
+  // Init node with name 'trackfinder'
+  ros::init(argc, argv, "test_svm");
+
+  ros::NodeHandle n;
+  ros::Publisher send_track = n.advertise<track::Cones>("/car/camera", 1000);
+
+
+  int num_points = 10;
+  float power = 1.25;
+
+  int a[10];
+
+  std::vector<track::Cone> new_cones;
+  track::Cones send_cones;
+
+  // track::Cone new_cones[num_points*2];
+  ROS_INFO("Generating fake track...");
+
+  for(int i=0; i<num_points; i++){
+
+    track::Cone coneleft;
+    track::Cone coneright;
+
+    coneleft.position.x = pow(i, power);
+    coneleft.position.y = i;
+    coneleft.color = 0;
+
+    coneright.position.x = pow(i, power) + 10;
+    coneright.position.y = i;
+    coneright.color = 1;
+
+    new_cones.push_back(coneleft);
+    new_cones.push_back(coneright);
+
+    ROS_INFO("%f, %f", coneleft.position.x, coneleft.position.y);
+  }
+
+  ROS_INFO("Generated fake track");
+
+  send_cones.cones = new_cones;
+  send_track.publish(send_cones);
+
+  return 0;
+}
