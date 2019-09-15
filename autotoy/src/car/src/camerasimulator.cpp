1d62e71a919cce63f259ef8680c2c59e9ff47fad
Pietro Campolucci <piecampo98@gmail.com>
2019-09-15 20:20:10 +0200

    Finalized the camera detector code

--- autotoy/src/car/src/camerasimulator.cpp
+++ autotoy/src/car/src/camerasimulator.cpp
@@ -5,12 +5,14 @@
 // and of the cones and publishes the recognized cones
 //
 
+// what it publishes: track::Cones through topic visible_cones
+// what it subscribe: track::Cones from cone_world (list of cones, only on time)
+//                    car::Location from car_state (current car position, multiple times)
+
 // including packages
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "track/Cones.h"
-#include "track/Cone.h"
-#include "track/Line.h"
+#include "car/Location.h"
 #include <math.h>
 #include <sstream>
 
@@ -18,11 +20,11 @@ using namespace std;
 
 // these parameters define the design of the camera of the car
 int fov = 120; //degrees
-int dof = 30;
+int dof = 30; //meters
 int acc = 100;
 
 // this class will get the position of the car and a list of cones, and will report a list of detected cones
-class car
+class bullet
 {
 public: //variables
 
@@ -39,16 +41,16 @@ public: //variables
         pos.push_back(tail_y);
         return pos;
     }
-    vector<float> activate(float fov, float angle, float dof, float acc, pair <float,float> position)
+    vector<float> activate(float fov, float dof, float acc, car::Location position)
     {
         vector<float> point_lst;
         float ang0;
         ang0 = -(fov/2);
         while(ang0 < fov/2)
         {
-            float real = angle + ang0;
-            float pointx = (position.first + dof*sin(real));
-            float pointy = (position.second + dof*cos(real));
+            float real = position.heading + ang0;
+            float pointx = (position.location.x + dof*sin(real));
+            float pointy = (position.location.y + dof*cos(real));
             point_lst.push_back(pointx);
             point_lst.push_back(pointy);
             ang0 = ang0 + (fov/acc);
@@ -56,70 +58,73 @@ public: //variables
         return point_lst;
 
     }
-    vector<float> detect(float conex, float coney, vector<float> lst, pair <float,float> position, float acc, float color)
+    bool detect(track::Cone cone, vector<float> lst, car::Location position, float acc)
     {
-        float cone_loc[2] = {conex, coney};
+        float cone_loc[2] = {cone.position.x, cone.position.y};
         float ind = 0;
         float cone_found = 0;
         while(ind < acc)
         {
             float p_x = lst[ind];
             float p_y = lst[ind+1];
-            float distance = abs((p_y-position.second)*conex - (p_x-position.first)*coney + p_x*position.second - p_y*position.first)
-            /sqrt(pow((p_y-position.second), 2)+pow((p_x-position.first), 2));
+            float distance = abs((p_y-position.location.y)*cone.position.x - (p_x-position.location.x)*cone.position.y + p_x*position.location.y - p_y*position.location.x)
+            /sqrt(pow((p_y-position.location.y), 2)+pow((p_x-position.location.x), 2));
             if(distance < 0.1)
             {        
                 cout << "cone detected" << endl;
-                vector<float> cone_location;
-                cone_location.push_back(conex);
-                cone_location.push_back(coney);
-                cone_location.push_back(color); // cone color, to be implemented
-                return cone_location;
-                break;
+                return true;
             } 
             ind = ind + 2;
         }
         cout << "cone not detected" << endl;
-        vector<float> nothing;
-        return nothing;
+        return false;
     }
 };
 
-// DEMO DATA: not for the final code, just to test if the code works
-float cones[3] = {50, 50, 1}; // posx, posy and color of the cone
-pair <float,float> position = make_pair (50, 50);
-float orientation = 0.5; // radians, respect to the y axis
+ros::Publisher camera_pub;
+track::Cones cones_lst;
 
+void carstateCallback(const car::Location& msg_car)
+{
+    bullet dut;
+    vector<float> view_lst = dut.activate(fov, dof, acc, msg_car);
+    ROS_INFO("New car position, detecting new cones...");
+
+    track::Cones detected_cones;
+
+    for(const track::Cone& cone : cones_lst.cones){
+        bool detection = dut.detect(cone, view_lst, msg_car, acc);
+        if(detection)
+        {
+            detected_cones.cones.push_back(cone);
+        }
+    }   
+    camera_pub.publish(detected_cones);
+}
+
+void coneworldCallback(const track::Cones& msg_cones)
+{   
+    ROS_INFO("Cones arrived!");
+    cones_lst = msg_cones;
+}
 
 int main(int argc, char **argv)
 {
     // initialize the car
-    car dut;
-
-    vector<float> view_lst = dut.activate(fov, orientation, dof, acc, position);
-    vector<float> detection = dut.detect(cones[0], cones[1], view_lst, position, acc, cones[2]);
 
     ros::init(argc, argv, "camerasimulator");
     ros::NodeHandle n;
-    ros::Publisher chatter_pub = n.advertise<track::Cones>("chatter", 1000); // here we specify the nature of the topic and its name
+    camera_pub = n.advertise<track::Cones>("visible_cones", 1000); // here we define what the topic is publishing and its name
+    n.subscribe("car_state", 1000, carstateCallback);
+    n.subscribe("cone_world", 1000, coneworldCallback);
     ros::Rate loop_rate(10);
 
     int count = 0;
     while (ros::ok())
     {
-        track::Cone cone;
-        cone.position.x = detection[0];
-        cone.position.y = detection[1];
-        cone.color = detection[2];
-
-        track::Cones detected_cones;
-        detected_cones.cones.push_back(cone);
-        
-        chatter_pub.publish(detected_cones);
         ros::spinOnce();
         loop_rate.sleep();
         ++count;
     }
-
     return 0;
 }
