10f8e78d86aa207da483f26bbe352cfafa8785c0
mvanlobensels <mees.van.loben.sels@dockbite.nl>
2019-09-15 20:58:15 +0200

    working talker and listener, still using dummy data

--- autotoy/src/car/src/controller.py
+++ autotoy/src/car/src/controller.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python
 import rospy
-from std_msgs import String
 from track.msg import Line
 from car.msg import Control, Location
 
@@ -9,45 +9,49 @@ class Controller:
         self.target_line_angle: float
         self.heading_angle: float
         
+        # Initialise node
+        rospy.init_node('controller')
+        
         # Inititate publisher and subscribers
-        self.listener()
+        self.publisher = rospy.Publisher('/car/controls', Control, queue_size=10)
         try:
-            self.talker()
+            self.listener()
         except rospy.ROSInterruptException:
             pass
         
-    def listener(self):
-        rospy.init_node('controller')
-        
+    def listener(self):        
         rospy.Subscriber('/car/targetline', Line, self.target_line_callback)
         rospy.Subscriber('/car/location', Location, self.location_callback)
         
+        print('Listening')
         rospy.spin()
         
     def talker(self):    
-        error = self.target_line_angle - self.heading_angle
+        # error = self.target_line_angle - self.heading_angle
         
         # Convert error to yaw rate
         # PID stuff...
+        acceleration = 4
+        yaw_rate = 1
         
-        publisher = rospy.Publisher('/car/controls', String)
-        rospy.init_node('')
-        
+        self.publisher.publish(acceleration, yaw_rate)        
               
     def target_line_callback(self, data):
         # Log data to console
-        rospy.loginfo(data.data)
+        rospy.loginfo(data)
     
         # Handle targetline data
         # Convert target line x,y position to angle with respect to reference coordinate system
-        self.target_line_angle = calculated_angle
+        # self.target_line_angle = calculated_angle
+        self.talker()
     
     def location_callback(self, data):
         # Log data to console
-        rospy.loginfo(data.data)
+        rospy.loginfo(data)
         
         # Handle location and heading data
-        self.heading_angle = heading_angle_from_data
+        # self.heading_angle = heading_angle_from_data
+        self.talker()
 
 
 if __name__ == '__main__':    
