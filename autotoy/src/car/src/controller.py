f15e91c4c63abd25088987d91fe537e043aba9d5
PL <p.lippmann@student.tudelft.nl>
2019-09-16 12:11:30 +0200

    Added acceleration calc

--- autotoy/src/car/src/controller.py
+++ autotoy/src/car/src/controller.py
@@ -27,12 +27,35 @@ class Controller:
         rospy.spin()
         
     def talker(self):    
-        # error = self.target_line_angle - self.heading_angle
+        error = self.target_line_angle - self.heading_angle
         
         # Convert error to yaw rate
-        # PID stuff...
-        acceleration = 4
         yaw_rate = 1
+        
+        # PID stuff... pip install simple-pid
+        from simple_pid import PID
+        pid = PID(1, 0.1, 0.05, setpoint=1)
+
+        # assume we have a system we want to control in controlled_system
+        v = controlled_system.update(0)
+
+        while True:
+        # compute new ouput from the PID according to the systems current value
+        control = pid(v)
+        # feed the PID output to the system and get its current value
+        v = controlled_system.update(control)
+
+        pid.sample_time = 0.01  # update every 0.01 seconds
+
+        while True:
+            output_yaw = pid(v)
+
+
+        acceleration = 4
+        if straighline:
+            acceleration =+ .1
+        else:
+            acceleration = 0
         
         self.publisher.publish(acceleration, yaw_rate)        
               
