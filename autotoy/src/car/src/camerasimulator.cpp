e09abffce264683f3b6a0a9b1ae1b9d9c146513d
Pietro Campolucci <piecampo98@gmail.com>
2019-09-20 22:24:16 +0200

    fixed camerasimulator, importing test code

--- autotoy/src/car/src/camerasimulator.cpp
+++ autotoy/src/car/src/camerasimulator.cpp
@@ -1,4 +1,12 @@
-//
+#include "ros/ros.h"
+#include "track/Cones.h"
+#include "car/Location.h"
+#include <math.h>
+#include <sstream>
+
+using namespace std;
+
+// =============================================================================================
 // Camera Simulator Publisher 
 // author: Pietro Campolucci
 // funtion: the code gathers the location of the car
@@ -8,71 +16,53 @@
 // what it publishes: track::Cones through topic visible_cones
 // what it subscribe: track::Cones from /track/cones (list of cones, only on time)
 //                    car::Location from /car/location (current car position, multiple times)
-
-// including packages
-#include "ros/ros.h"
-#include "track/Cones.h"
-#include "car/Location.h"
-#include <math.h>
-#include <sstream>
-
-using namespace std;
+// =============================================================================================
 
 // these parameters define the design of the camera of the car
-int fov = M_PI; //radians
-int dof = 5; //meters
-int acc = 300;
+float fov = M_PI/2; //radians
+float dof = 20; //meters
+float acc = 200; // NOTE: if you decrease the accuracy, you need to increase the max distance for detection
 
 // this class will get the position of the car and a list of cones, and will report a list of detected cones
 class bullet
 {
-public: //variables
+public:
 
-    vector<float> get_orientation(float angle, pair <float,float> position)
-    {
-        float nose_x = position.first + 1.5*sin(angle); //add here core using angle
-        float nose_y = position.second + 1.5*cos(angle);
-        float tail_x = position.first - 1.5*sin(angle);
-        float tail_y = position.second - 1.5*cos(angle);
-        vector<float> pos;
-        pos.push_back(nose_x);
-        pos.push_back(nose_y);
-        pos.push_back(tail_x);
-        pos.push_back(tail_y);
-        return pos;
-    }
+    // plotting field of view as an arc of points (returns a list of points)
     vector<float> activate(float fov, float dof, float acc, car::Location position)
     {
         vector<float> point_lst;
         float ang0;
-        ang0 = +(fov/2);
-        while(ang0 > -fov/2)
+        ang0 = -(fov/2);
+        while(ang0 < fov/2)
         {
             float real = position.heading + ang0;
             float pointx = (position.location.x + dof*cos(real));
             float pointy = (position.location.y + dof*sin(real));
             point_lst.push_back(pointx);
             point_lst.push_back(pointy);
-            ang0 = ang0 - (fov/acc);
+            ang0 = ang0 + fov/acc;
         }
         return point_lst;
-
     }
+
+    // gets the coord of one cone and returns if it is detected or not (returns 1 for "yes" and 0 for "no")
+    // NOTE: the print out messages will be reported once the node receives cones positions
     bool detect(track::Cone cone, vector<float> lst, car::Location position, float acc)
     {
         float cone_loc[2] = {cone.position.x, cone.position.y};
         float ind = 0;
         float cone_found = 0;
-        while(ind < acc)
+        while(ind < lst.size())
         {
             float p_x = lst[ind];
             float p_y = lst[ind+1];
-            float distance = abs((p_y-position.location.y)*cone.position.x - (p_x-position.location.x)*cone.position.y + p_x*position.location.y - p_y*position.location.x)
-            /sqrt(pow((p_y-position.location.y), 2)+pow((p_x-position.location.x), 2));
-            if(distance < 0.05)
+            float distance = abs((p_y-position.location.y)*cone.position.x - (p_x-position.location.x)*cone.position.y + p_x*position.location.y - p_y*position.location.x)/sqrt(pow((p_y-position.location.y), 2)+pow((p_x-position.location.x), 2));
+            if(distance < 0.1)
             {        
                 cout << "cone detected" << endl;
                 return true;
+                break;
             } 
             ind = ind + 2;
         }
@@ -84,6 +74,7 @@ public: //variables
 ros::Publisher camera_pub;
 track::Cones cones_lst;
 
+// get the car location and publish the list of new cones
 void carstateCallback(const car::Location& msg_car)
 {
     bullet dut;
@@ -98,10 +89,12 @@ void carstateCallback(const car::Location& msg_car)
         {
             detected_cones.cones.push_back(cone);
         }
-    }   
+    } 
+    ROS_INFO("SEnding position of all the detected cones");  
     camera_pub.publish(detected_cones);
 }
 
+// get cones location just one time for further use
 void coneworldCallback(const track::Cones& msg_cones)
 {   
     ROS_INFO("Cones arrived!");
@@ -110,11 +103,12 @@ void coneworldCallback(const track::Cones& msg_cones)
 
 int main(int argc, char **argv)
 {
-    // initialize the car
-
+    // initialize ros
     ros::init(argc, argv, "camerasimulator");
     ros::NodeHandle n;
-    camera_pub = n.advertise<track::Cones>("/car/camera", 1000); // here we define what the topic is publishing and its name
+
+    // publish cones location by car loaction info and cones location
+    camera_pub = n.advertise<track::Cones>("/car/camera", 1000); 
     ros::Subscriber a = n.subscribe("/car/location", 1000, carstateCallback);
     ros::Subscriber b = n.subscribe("/track/cones", 1000, coneworldCallback);
     ros::spin();
--- autotoy/src/car/src/test_camerasimulator.cpp
+++ autotoy/src/car/src/test_camerasimulator.cpp
@@ -1,34 +1,29 @@
-
 #include <vector>
 #include <math.h>
 #include <sstream>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-// these parameters define the design of the camera of the car
-int fov = 120; //degrees
-int dof = 30;
-int acc = 100;
+// =============================================================
+// Test Code for Camera Simulation
+// Description: the code will send the cone coordinates to
+// cone_loc.txt and the fov coordinates to fov_loc.txt, for 
+// further car visualization
+// =============================================================
 
-// this class will get the position of the car and a list of cones, and will report a list of detected cones
+// these parameters define the design of the camera of the car
+float fov = M_PI/2; //radians
+float dof = 30; //meters
+float acc = 200; //number of lines
+
+// the class will provide functions to plot the fov and use it to detect the cones in the way
 class car
 {
-public: //variables
+public:
 
-    vector<float> get_orientation(float angle, pair <float,float> position)
-    {
-        float nose_x = position.first + 1.5*sin(angle); //add here core using angle
-        float nose_y = position.second + 1.5*cos(angle);
-        float tail_x = position.first - 1.5*sin(angle);
-        float tail_y = position.second - 1.5*cos(angle);
-        vector<float> pos;
-        pos.push_back(nose_x);
-        pos.push_back(nose_y);
-        pos.push_back(tail_x);
-        pos.push_back(tail_y);
-        return pos;
-    }
+    // plotting field of view as an arc of points (returns a list of points)
     vector<float> activate(float fov, float angle, float dof, float acc, pair <float,float> position)
     {
         vector<float> point_lst;
@@ -41,17 +36,19 @@ public: //variables
             float pointy = (position.second + dof*cos(real));
             point_lst.push_back(pointx);
             point_lst.push_back(pointy);
-            ang0 = ang0 + (fov/acc);
+            ang0 = ang0 + fov/acc;
         }
         return point_lst;
-
     }
+
+    // gets the coord of one cone and returns if it is detected or not (returns 1 for "yes" and 0 for "no")
+    // NOTE: the print out messages will be reported once the node receives cones positions
     float detect(float conex, float coney, vector<float> lst, pair <float,float> position, float acc)
     {
         float cone_loc[2] = {conex, coney};
         float ind = 0;
         float cone_found = 0;
-        while(ind < acc)
+        while(ind < lst.size())
         {
             float p_x = lst[ind];
             float p_y = lst[ind+1];
@@ -70,9 +67,9 @@ public: //variables
 };
 
 // DEMO DATA: not for the final code, just to test if the code works
-float cones[3] = {50, 50, 1}; // posx, posy and color of the cone
+float cones[3] = {30, 60, 1}; // posx, posy and color of the cone
 pair <float,float> position = make_pair (50, 50);
-float orientation = 0.5; // radians, respect to the y axis
+float orientation = M_PI*1.43; // radians, respect to the y axis, clockwise
 
 
 int main(int argc, char **argv)
@@ -80,10 +77,26 @@ int main(int argc, char **argv)
     // initialize the car
     car dut;
 
+    // get fov and use it to get the detection statement
     vector<float> view_lst = dut.activate(fov, orientation, dof, acc, position);
     float detection = dut.detect(cones[0], cones[1], view_lst, position, acc);
-    cout << detection << endl;
 
+    // store information of the point to cone_loc.txt
+    std::ofstream file;
+    file.open("cone_loc.txt");
+    file << 'x' << ' ' << 'y' << '\n';
+    file << cones[0] << ' ' << cones[1] <<std::endl;
+    file.close();
+
+    // store information of the camera field to fov_loc.txt
+    std::ofstream file_1;
+    file_1.open("fov_loc.txt");
+    int ind = 0;
+    file_1 << 'x' << ' ' << 'y' << '\n';
+    while(ind<(view_lst.size())){
+        file_1 << view_lst[ind] << ' ' << view_lst[ind+1] <<std::endl; 
+        ind = ind + 2;
+    }
+    file_1.close();
     return 0;
-
 }
